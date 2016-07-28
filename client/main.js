12eaef54f97b4eb71d55cb7b60abb60c140d42f8
cptran777 <cptran14@gmail.com>
2016-07-27 21:33:18 -0700

    progress update for broadcasts.

--- client/main.js
+++ client/main.js
@@ -1,12 +1,54 @@
+// Testing peer data connection broadcasts in an isolated environment. 
 
-/* videochatroom.js */
-// Compatibility shim
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 console.log('most updated version');
 // Start user
 var peer = new Peer({ key: 'lwjd5qra8257b9', debug: 0});
+
+const getMyId = () => new Promise((resolve, reject) => {
+  peer.on('open', resolve);
+  peer.on('error', reject);
+});
+
+var peerId; 
+
+const establishPeerConnection = (sourceId) => new Promise((resolve, reject) => {
+  const connect = () => {
+    if (sourceId) {
+      const conn = peer.connect(sourceid, { reliable: true });
+
+      conn.on('open', () => {
+        console.log('RTC data connection established - acting as receiver');
+        resolve(conn);
+      });
+
+      conn.on('error', (error) => {
+        reject(error);
+      });
+    } else {
+      peer.on('connection', (conn) => {
+        conn.on('open', () => {
+          console.log('RTC data connection established - acting as source');
+          resolve(conn);
+        });
+      });
+      peer.on('error', (error) => {
+        reject(error);
+      });
+    }
+  };
+
+  if (peer.disconnected) {
+    peer.on('open', connect);
+    peer.on('error', reject);
+  } else {
+    connect();
+  }
+});
+
 peer.on('open', function(){
   $('#mypeerid').append("Your peer id: " + peer.id);
+  peerId = peer.id;
 });
 peer.on('call', function(call){
   console.log("Call received");
